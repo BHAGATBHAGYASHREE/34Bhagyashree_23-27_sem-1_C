e70451449e8f3f5d57ec1ad6d19c6c45bbe886eb
BHAGATBHAGYASHREE <142775493+BHAGATBHAGYASHREE@users.noreply.github.com>
2023-12-22 22:14:35 +0530

    Add files via upload

---
+++ sumab.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+//calculate the sum of two number
+{
+    int a=2,b=3,sum;
+    sum=a+b;
+    printf("sum is = %d",sum);
+    return 0;
+}
---
+++ sumabc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter value of a and b ");
+    scanf("%d %d" ,&a,&b);
+
+    c=a+b;
+
+    printf("Addition is: %d",c);
+
+    return 0;
+
+}
